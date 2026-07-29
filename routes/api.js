f50ef33617a1da887f6d21adeb05c3c0f251a452
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-07-29 14:31:19 +0700

    feat: implement API routes for user authentication and comic management

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const komikController = require('../controller/komikController');
 const userController = require('../controller/userController');
 const authMiddleware = require('../middleware/authMiddleware');
+const genreController = require('../controller/genreController');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
@@ -12,9 +13,11 @@ router.post('/login', userController.login);
 router.get('/komik', komikController.getAllKomik);
 router.get('/komik/:id', komikController.getKomikById);
 
+
 //protected
 router.post('/komik', authMiddleware, komikController.createKomik);
 router.put('/komik/:id', authMiddleware, komikController.updateKomik);
 router.delete('/komik/:id', authMiddleware, komikController.deleteKomik);
+
 
 module.exports = router;
