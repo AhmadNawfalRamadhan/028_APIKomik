36170ff14abb58405054283b87ecd98e5a295b8d
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-07-29 14:33:19 +0700

    feat: add CRUD routes for genre management

--- routes/api.js
+++ routes/api.js
@@ -21,5 +21,10 @@ router.post('/komik', authMiddleware, komikController.createKomik);
 router.put('/komik/:id', authMiddleware, komikController.updateKomik);
 router.delete('/komik/:id', authMiddleware, komikController.deleteKomik);
 
+// Genre
+router.post('/genre', authMiddleware, genreController.createGenre);       
+router.put('/genre/:id', authMiddleware, genreController.updateGenre);    
+router.delete('/genre/:id', authMiddleware, genreController.deleteGenre);
+
 
 module.exports = router;
