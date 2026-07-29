928e44fbf676045e58b30dd6b065dac683a6223e
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-07-29 14:21:49 +0700

    menambahkan relasi genre pada model komik untuk mendefinisikan hubungan antara komik dan genre

--- models/komik.js
+++ models/komik.js
@@ -13,7 +13,21 @@ module.exports = (sequelize, DataTypes) => {
         },
         author: {
             type: DataTypes.STRING,
+        },
+        // TAMBAHAN: Kolom untuk menyimpan ID dari Genre
+        genre_id: {
+            type: DataTypes.INTEGER,
         }
     });
+
+    // Mendefinisikan relasi ke tabel Genre
+    komik.associate = function(models) {
+        // 1 Komik dimiliki oleh (belongsTo) 1 Genre
+        komik.belongsTo(models.genre, {
+            foreignKey: 'genre_id',
+            as: 'genre' // alias untuk pemanggilan (opsional tapi disarankan)
+        });
+    };
+
     return komik;
 };
